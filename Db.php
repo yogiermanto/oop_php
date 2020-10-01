25347787d500fd64b4244cce35229cc88efea75b
Yogi Ermanto <yogiermanto59@gmail.com>
2020-10-01 08:43:35 +0700

    finish

--- Db.php
+++ Db.php
@@ -46,6 +46,11 @@ class Db
       */
       private $_orderBy = "";
 
+    /**
+      * @var int
+      */
+      private $_count = 0;
+
     private function __construct()
     {
         try {
@@ -120,13 +125,16 @@ class Db
      * Run the select statemen based on the $tableName parameter
      * 
      * 
-     * @param string $query
+     * @param string $tableName
+     * @param string $condition
+     * @param array $bindValue
+     * @param string $limit
      * 
      * @return object
      */
-    public function get(string $tableName, string $condition = "", array $bindValue = [])
+    public function get(string $tableName, string $condition = "", array $bindValue = [], string $limit = "")
     {
-        $query = "SELECT {$this->_columnName} FROM {$tableName} {$condition} {$this->_orderBy}";
+        $query = "SELECT {$this->_columnName} FROM {$tableName} {$condition} {$this->_orderBy} {$limit}";
         $this->_columnName = "*";
         $this->_orderBy = "";
         return $this->getQuery($query, $bindValue);
@@ -174,9 +182,9 @@ class Db
     }
 
     /**
-     * Order By
+     * Get Where
      * 
-     * Generate query order by based on parameter $columnName and sortType
+     * Generate query get with where condition
      * 
      * 
      * @param string $tableName
@@ -190,5 +198,161 @@ class Db
 
       return $this->get($tableName, $queryCondition, [$condition[2]]);
     }
+
+    /**
+     * Get Where Once
+     * 
+     * Generate query get with where condition then only return single object
+     * 
+     * 
+     * @param string $tableName
+     * @param array $condition
+     * 
+     * @return object
+     */
+    public function getWhereOnce(string $tableName, array $condition)
+    {
+      $queryCondition = "WHERE {$condition[0]} {$condition[1]} ?";
+
+      $result = $this->get($tableName, $queryCondition, [$condition[2]], 'LIMIT 1');
+
+      if (empty($result)) {
+        return false;
+      }
+
+      return $result[0];
+    }
+
+     /**
+     * Get Like
+     * 
+     * Generate query get with where like condition
+     * 
+     * 
+     * @param string $tableName
+     * @param string $columnLike
+     * @param string $search
+     * 
+     * @return object
+     */
+    public function getLike(string $tableName, string $columnLike, string $search)
+    {
+      $queryCondition = "WHERE {$columnLike} LIKE ?";
+      
+      return $this->get($tableName, $queryCondition, [$search]);
+    }
+
+     /**
+     * Get Like
+     * 
+     * Generate query get with where like condition
+     * 
+     * 
+     * @param string $tableName
+     * @param string $columnLike
+     * @param string $dataValues
+     * 
+     * @return int
+     */
+    public function check(string $tableName, string $columnName, string $dataValues)
+    {
+      $query = "SELECT {$columnName} FROM {$tableName} WHERE {$columnName} = {$dataValues}";
+      
+      return $this->runQuery($query)
+                  ->rowCount();
+    }
+
+    /**
+     * Insert
+     * 
+     * Generate query insert base on parameters
+     * 
+     * 
+     * @param string $tableName
+     * @param array $data
+     * 
+     * @return bool
+     */
+    public function insert(string $tableName, array $data)
+    {
+      $listColumn = "(".implode(', ', array_keys($data)).")";
+      $placeHolder = "(".str_repeat('?, ', count($data)-1)."? )";
+
+      $query = "INSERT INTO {$tableName} {$listColumn} VALUES {$placeHolder}";
+      $this->_count = $this->runQuery($query, array_values($data))
+                           ->rowCount();
+      return true;
+    }
+
+    /**
+     * Count affected rows
+     * 
+     * Getter method to count total affected rows
+     * 
+     * 
+     * @param void
+     * 
+     * @return int
+     */
+    public function count()
+    {
+      return $this->_count;
+    }
+
+    /**
+     * Update
+     * 
+     * Generate query update base on parameters
+     * 
+     * 
+     * @param string $tableName
+     * @param array $data
+     * @param array $condition
+     * 
+     * @return bool
+     */
+    public function update(string $tableName, array $data, array $condition)
+    {
+		$query = "UPDATE {$tableName} SET ";
+		foreach ($data as $key => $value) {
+			$query .= "{$key} = ?, ";
+		}
+
+		$query = substr($query, 0, -2);
+		$query .= " WHERE {$condition[0]} {$condition[1]} ?";
+
+		$data_values = array_values($data);
+		array_push($data_values, $condition[2]);
+		
+		$this->_count = $this->runQuery($query, $data_values)
+							 ->rowCount();
+
+		return true;
+	}
+	
+	 /**
+     * Delete
+     * 
+     * Generate query delete base on parameters
+     * 
+     * 
+     * @param string $tableName
+     * @param array $condition
+     * 
+     * @return bool
+     */
+    public function delete(string $tableName, array $condition)
+    {
+		$query = "DELETE FROM {$tableName} WHERE {$condition[0]} {$condition[1]} ?";
+		$this->_count = $this->runQuery($query, [$condition[3]])
+							 ->rowCount();
+
+		return true;
+    }
+
+
+
+
+
 
 }
--- index.php
+++ index.php
@@ -12,9 +12,41 @@ $Db = Db::getInstance();
 // $result = $Db->runQuery($query, $bindValue);
 
 // $result = $Db->getQuery("SELECT * FROM barang");
-$result = $Db->select('id_barang, nama_barang')
-             ->select('jumlah_barang')
-             ->orderBy('jumlah_barang', 'DESC')
-             ->getWhere('barang', ['id_barang', '=', '5']);
+// $result = $Db->select('id_barang, nama_barang')
+//              ->select('jumlah_barang')
+//              ->orderBy('jumlah_barang', 'DESC')
+//              ->getWhere('barang', ['id_barang', '=', '5']);
 
-var_dump($result);
+// $result = $Db->select('nama_barang, jumlah_barang')
+//              ->getLike('barang', 'nama_barang', '%k%');
+
+// if ($Db->check('barang', 'id_barang', '10')) {
+//     echo 'ID Barang 10 tersedia';
+// }
+
+// $result = $Db->insert('barang', [
+//             'nama_barang' => 'Philips Blender',
+//             'jumlah_barang' => 11,
+//             'harga_barang' =>629000
+//          ]);
+
+// if ($result) {
+//     echo "Terdapat {$Db->count()} data yang ditambahkan!";
+// }
+
+// $tableBarang = $Db->get('barang');
+
+// $result = $Db->update('barang', ['nama_barang' => 'Dummy Project', 'harga_barang' => 99999], ['id_barang', '=', 3]);
+
+// if ($result) {
+//     echo "Terdapat {$Db->count()} data yang diubah";
+// }
+
+$result = $Db->delete('barang', ['id_barang', '<', 5]);
+
+if ($result)
+{
+    echo "Terdapat {$Db->count()} data yang diubah";
+}
+// echo $result->id_barang;
+// var_dump($tableBarang);
